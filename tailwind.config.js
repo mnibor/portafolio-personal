83380c4930137f1ddc1230d1303726ca2ddef2e0
mnibor <marcelorobin@gmail.com>
2024-12-05 13:00:05 -0300

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+}
