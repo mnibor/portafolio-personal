4618a0b9774aa6cd67aea872b5177fdcb39d0fb8
mnibor <marcelorobin@gmail.com>
2024-12-11 19:46:47 -0300

    ultimos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from 'astro/config'
+import astroIcon from 'astro-icon'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		astroIcon({
+			include: ['mdi'], // Esto es lo importante
+		}),
+	],
+})
